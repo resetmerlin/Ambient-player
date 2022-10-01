55b983ebdf89d381dfab7d0817b86e4f8b9b114f
resetmerlin <108568153+resetmerlin@users.noreply.github.com>
2022-10-02 06:58:53 +0900

    2022/10/02

---
+++ js/howlerImport.js
@@ -0,0 +1,37 @@
+// const soundtrack1 = new Howl({
+//   src: ["songs/chill.mp3"],
+//   volume: 0.3,
+// });
+
+// const soundtrack2 = new Howl({
+//   src: ["songs/heavenly.mp3"],
+//   volume: 0.3,
+// });
+
+// const soundtrack3 = new Howl({
+//   src: ["songs/peace.mp3"],
+//   volume: 0.3,
+// });
+
+// let songPlaying = false;
+
+// function playSong() {
+//   songPlaying = true;
+//   loadSong(soundtrack1);
+
+//   soundtrack1.play();
+//   playPause.classList.add("active");
+//   playPause.innerHTML = `<ion-icon name="pause-outline"></ion-icon>`;
+// }
+// function pauseSong() {
+//   songPlaying = false;
+//   audio.pause();
+//   playPause.classList.remove("active");
+//   playPause.innerHTML = `<ion-icon name="play-outline"></ion-icon>`;
+// }
+
+// function loadSong(songList) {
+//   title.textContent = songList.songName;
+//   audio.src = songList.path;
+//   changeVid(i);
+// }
--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const playPause = document.querySelector(".playPause");
 const next = document.querySelector(".next");
 const audio = document.querySelector(".audio");
 const video = document.querySelector(".wallpaper");
+const rain = document.querySelector(".rain");
+const thunder = document.querySelector(".thunder");
+const wind = document.querySelector(".wind");
 
 const songList = [
   {
@@ -21,9 +24,94 @@ const songList = [
     songName: "Peace",
   },
 ];
-
+let id;
 let songPlaying = false;
+let rainPlaying = false;
+let thunderPlaying = false;
+let windPlaying = false;
 
+//play rain
+
+function playRainPause() {
+  rainPlaying = false;
+
+  var sound = new Howl({
+    src: ["/songs/rain.mp3"],
+    volume: 0.1,
+    loop: true,
+  });
+
+  Howler.stop();
+  rain.classList.remove("active");
+}
+
+function playRainPlay() {
+  rainPlaying = true;
+
+  var sound = new Howl({
+    src: ["/songs/rain.mp3"],
+    volume: 0.1,
+    loop: true,
+  });
+
+  sound.play();
+  rain.classList.add("active");
+}
+
+//play storm
+
+function playStormPause() {
+  thunderPlaying = false;
+
+  var sound2 = new Howl({
+    src: ["/songs/thunder1.mp3", "/songs/thunder2.mp3"],
+    volume: 0.5,
+    loop: true,
+  });
+
+  Howler.stop();
+  thunder.classList.remove("active");
+}
+
+function playStormPlay() {
+  thunderPlaying = true;
+
+  var sound2 = new Howl({
+    src: ["/songs/thunder1.mp3", "/songs/thunder2.mp3"],
+    volume: 0.5,
+    loop: true,
+  });
+
+  sound2.play();
+  thunder.classList.add("active");
+}
+
+//play wind
+function playWindPause() {
+  windPlaying = false;
+
+  var sound1 = new Howl({
+    src: ["/songs/wind.mp3"],
+    volume: 0.1,
+    loop: true,
+  });
+
+  Howler.stop();
+  wind.classList.remove("active");
+}
+
+function playWindPlay() {
+  windPlaying = true;
+
+  var sound1 = new Howl({
+    src: ["/songs/wind.mp3"],
+    volume: 0.1,
+    loop: true,
+  });
+
+  sound1.play();
+  wind.classList.add("active");
+}
 //play song
 function playSong() {
   songPlaying = true;
@@ -41,9 +129,22 @@ function pauseSong() {
   playPause.innerHTML = `<ion-icon name="play-outline"></ion-icon>`;
 }
 
+function playRain() {
+  rainPlaying ? playRainPause() : playRainPlay();
+}
+function playThunder() {
+  thunderPlaying ? playStormPause() : playStormPlay();
+}
+
+function playWind() {
+  windPlaying ? playWindPause() : playWindPlay();
+}
 //play or pause song on click
 
 playPause.addEventListener("click", kt);
+rain.addEventListener("click", playRain);
+thunder.addEventListener("click", playThunder);
+wind.addEventListener("click", playWind);
 
 function kt() {
   songPlaying ? pauseSong() : playSong();
@@ -52,13 +153,18 @@ function kt() {
 function loadSong(songList) {
   title.textContent = songList.songName;
   audio.src = songList.path;
-  changeVid(event);
+  changeVid(i);
 }
 
-function changeVid(event) {
+function changeVid(i) {
   event.preventDefault();
 
   let type = video.src;
+  if (i == 0) {
+    if (type !== `img/mylivewallpapers.com-Lo-Fi-Girl.mp4`) {
+      video.src = `img/mylivewallpapers.com-Lo-Fi-Girl.mp4`;
+    }
+  }
   if (i == 1) {
     if (type !== `img/livingroom.mp4`) {
       video.src = `img/livingroom.mp4`;
@@ -83,12 +189,19 @@ function prevSong() {
 prev.addEventListener("click", prevSong);
 
 function nextSong() {
-  i++;
-  if (i < 0) {
-    i = songList.length + 1;
+  if (i !== 2) {
+    i++;
+    if (i < 0) {
+      i = songList.length + 1;
+    }
+
+    loadSong(songList[i]);
+    playSong();
+  } else if (i == 2) {
+    i === 0;
+    loadSong(songList[i]);
+    playSong();
   }
-  loadSong(songList[i]);
-  playSong();
 }
 
 next.addEventListener("click", nextSong);
